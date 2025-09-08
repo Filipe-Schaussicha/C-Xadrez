08c46b26933f0bfeccef501af78183c52d21bad3
Filipe <filipeschausst@gmail.com>
2025-09-08 12:21:56 -0300

    Quadrados do tabuleiro são destacados quando o mouse entra em contato

--- code/visual.c
+++ code/visual.c
@@ -12,16 +12,20 @@ void DesenharTabuleiro(){
     Color branco = RAYWHITE;
     Color preto = (Color){100, 100, 100, 255};
 
+    Vector2 mouse = GetMousePosition();
+
     for(int linha = 0; linha < 8; linha++){
         for(int coluna = 0; coluna < 8; coluna++){
 
             Color cor = ((linha + coluna) % 2 == 0) ? branco : preto;
 
-            int x = inicio.x + tamanho_quadrado * coluna;
-            int y = inicio.y + tamanho_quadrado * linha;
+            Rectangle quadrado = (Rectangle){inicio.x + tamanho_quadrado * coluna, inicio.y + tamanho_quadrado * linha, tamanho_quadrado, tamanho_quadrado};
 
-            DrawRectangle(x, y, tamanho_quadrado, tamanho_quadrado, cor);
+            DrawRectangleRec(quadrado, cor);
+
+            if(CheckCollisionPointRec(mouse, quadrado)){
+                DrawRectangleLinesEx(quadrado, tamanho_quadrado * 0.05, RED);
+            }
         }
     }
-
 }
